eba0e02349d27e0028ec622233e045bdfa49e1a5
onkar-code <cf19onkar.singh@iesjoandaustria.org>
2021-02-01 20:35:28 +0100

    operaciones de la calculador

--- calc.py
+++ calc.py
@@ -8,12 +8,35 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/suma/<param1>/<param2>')
-def suma(param1, param2)
+@app.route('/calc/suma/<param1>/<param2>')
+def suma(param1, param2):
     n_param1 = float(param1)
     n_param2 = float(param2)
     resultat = {'resultat':n_param1+n_param2}
     return jsonify(resultat), 200
+
+@app.route('/calc/resta/<param1>/<param2>')
+def resta(op1, op2):
+	n_param1 = float(param1)
+	n_param2 = float(param2)
+    
+	resultat = {'resultat': n_param1 - n_param2}   	
+	return jsonify(resultat), 200
+	
+@app.route('/calc/multiplicacio/<param1>/<param2>')
+def multiplicacio(param1, param2):
+	n_param1 = float(param1)
+	n_param2 = float(param2)
+	resultat = {'resultat': n_param1 * n_param2}    
+	return jsonify(resultat), 200
+	
+@app.route('/calc/divisio/<param1>/<param2>')
+def divisio(param1, param2):
+	n_param1 = float(param1)
+	n_param2 = float(param2)
+	resultat = {'resultat': n_param1 / n_param2}    
+	return jsonify(resultat), 200
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
