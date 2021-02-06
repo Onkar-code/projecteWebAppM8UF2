b21d182a1348ad17b4b5740f34801475849fb07f
onkar-code <cf19onkar.singh@iesjoandaustria.org>
2021-02-06 20:22:31 +0100

    updated

--- calc.py
+++ calc.py
@@ -7,26 +7,26 @@ app = Flask(__name__)
 
 @app.route('/', methods=['POST','GET'])
 def login():
-    error = None
-    if request.method=='POST':
-    	if request.form['user'] != 'user' or request.form['pwd'] != '1234':
-    		error = 'Invalid credentials. Please try again.'
-    	else:
-    		return render_template('calc.html')
-    return render_template('index.html',error=error)
+	error = None
+	if request.method=='POST':
+		if request.form['user'] != 'user' or request.form['pwd'] != '1234':
+			error = 'Invalid credentials. Please try again.'
+		else:
+			return render_template('calc.html')
+	return render_template('index.html',error=error)
     
 @app.route('/calc/suma/<param1>/<param2>')
 def suma(param1, param2):
-    n_param1 = float(param1)
-    n_param2 = float(param2)
-    resultat = {'resultat':n_param1+n_param2}
-    return jsonify(resultat), 200
-
-@app.route('/calc/resta/<param1>/<param2>')
-def resta(op1, op2):
 	n_param1 = float(param1)
 	n_param2 = float(param2)
-    
+	resultat = {'resultat':n_param1+n_param2}
+	return jsonify(resultat), 200
+
+@app.route('/calc/resta/<param1>/<param2>')
+def resta(param1, op2param2):
+	n_param1 = float(param1)
+	n_param2 = float(param2)
+
 	resultat = {'resultat': n_param1 - n_param2}   	
 	return jsonify(resultat), 200
 	
